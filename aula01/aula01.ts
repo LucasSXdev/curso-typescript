196dd89df3bba49010374ba7678883cdf7f859d6
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2024-03-12 22:43:10 -0300

    adicionando a aula2

---
+++ aula01/aula01.ts
@@ -0,0 +1,17 @@
+/*
+const spaceship ={
+    name:"X-wing",
+    number:0
+}Ao passar com o mouse sobre a variável spaceship é possível ver que a IDE já inferiu os tipos string e number. Mas é possível fazer mais.
+
+
+//Passe o mouse sobre os parâmetros da função e clique em "Correção rápida" e "Inferir todos os tipos de uso". Seu código agora estará assim:
+function accelerate(spaceship: { name: string; number: number },speed: number){
+    spaceship.number = speed; 
+}
+
+accelerate(spaceship,50)
+*/
+
+
+
