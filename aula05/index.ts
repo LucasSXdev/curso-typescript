f05e99accd1f8ded4aaeaeb37deed20f9f0e5cf1
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2024-03-16 00:00:45 -0300

    adicionando o código da aula05

---
+++ aula05/index.ts
@@ -0,0 +1,13 @@
+//Enums
+//conjunto de constantes que podemos atribuir nomes
+//pode ser referenciado mais facilmente
+
+enum planets{//CADA ELEMENTO DENTRO DESSE ENUM TEM UM NÚMERO E COMEÇA DO 0
+    MERCURIO,
+    VENUS,
+    TERRA,
+    MARTE
+}
+
+planets.MERCURIO//ESSA LINHA VAI RETORNAR UM NÚMERO
+
