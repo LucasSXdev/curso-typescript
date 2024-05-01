e1030704aeae7e7d7a9d50fc3554c8dd87b7ff24
Lucas <lucaswats39@gmail.com>
2024-04-30 23:02:06 -0300

    adicionando a função de atualizar e remover planetas

--- ex2/src/index.ts
+++ ex2/src/index.ts
@@ -1,19 +1,41 @@
-let planets = []
 
-function createPlanet(name:string,coordinates:[number,number,number,number],situation:"Habitado"|"Habitável"|"inabitavel"|"Inexplorado",satellite:string[]){
-    const planet = {
-        name,
-        coordinates,
-        situation,
-        satellite:[]
-    }
-
-    planets.push(planet)
-
-    alert(`O planeta ${planet.name} foi adicionado`)
-
+type planetSituation = 'Habitado'|'Habitável'|'Inabitável'|'inexplorado'
+type planetCordinates =[number,number,number,number]
+type planet = {
+    name:string,
+    cordinates:planetCordinates,
+    situation:planetSituation,
+    satellites:string[]
 }
+const planets : planet []=[]
 
-function updatePlanet(){
+function createPlanet(name:string,cordinates:planetCordinates,situation:planetSituation){
     
+    planets.push({
+        name,
+        cordinates,
+        situation,
+        satellites:[]
+    })
+
+    alert(`O planeta ${name} foi adicionado com sucesso`)
+
 }
+
+
+function findPlanet(name:string){
+    const planet = planets.find(plan=> plan.name === name)
+    return planet
+}
+
+function updatePlanet(situation:planetSituation,planet:planet){
+    planet.situation = situation
+    
+    alert(`A situação do planeta${planet.name} foi atualizada`)
+}
+
+function addSatellite(name,planet:planet){
+    planet.satellites.push[name]
+    alert(`o satelite${name}foi adicionado ao planeta ${planet.name}`)
+}
+
